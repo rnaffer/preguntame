d68f708df17fea5be254f81f83f5d8a4deb269ae
Naffer Reyes <reyesnaffer@gmail.com>
2015-01-10 01:32:12 -0430

    fix

--- preguntame/settings.py
+++ preguntame/settings.py
@@ -65,7 +65,11 @@ WSGI_APPLICATION = 'preguntame.wsgi.application'
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
